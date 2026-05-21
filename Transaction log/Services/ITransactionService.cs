d2a6622401a027a4db9aa2cb4be622a3b4b6c16d
Singh <2492266@cognizant.com>
2026-05-21 16:26:23 +0530

    add Transaction file

---
+++ Transaction log/Services/ITransactionService.cs
@@ -0,0 +1,11 @@
+﻿using Transaction_log.DTOs;
+
+namespace Transaction_log.Services
+{
+    public interface ITransactionService
+    {
+        Task<IEnumerable<TransactionLogResponse>> GetAllTransactionsAsync();
+        Task<TransactionLogResponse?> GetTransactionByIdAsync(int id);
+        Task<TransactionLogResponse> CreateTransactionAsync(CreateTransactionRequest request);
+    }
+}
