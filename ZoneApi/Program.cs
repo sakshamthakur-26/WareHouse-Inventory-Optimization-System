3ea793446082816f0e43b81d1365a50f555df87f
Kar <2492170@cognizant.com>
2026-05-21 17:16:34 +0530

    added zone

---
+++ ZoneApi/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using Zone.Services;
+
+namespace ZoneApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddScoped<IZoneService, ZoneService>();
+
+            builder.Services.AddDbContext<Model.ZoneContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ZoneDb")));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ ZoneApi/Services/IZoneService.cs
@@ -0,0 +1,23 @@
+﻿using Zone.DTOs;
+namespace Zone.Services
+{
+    public interface IZoneService
+    {
+        Task<IEnumerable<ZoneResponse>> GetAllAsync();
+        Task<ZoneResponse> GetByIdAsync(int id); // why do we use IEnnumerable and when to ??
+
+        Task<ZoneResponse> CreateAsync(CreateZoneRequest request);
+        Task UpdateAsync(int id, CreateZoneRequest request);
+        Task DeleteAsync(int id);
+
+
+        ////////////////////////////////////////////////////
+        //                 CAPACITY BASED
+
+
+        Task<bool> CheckAvailableCapacityAsync(int zoneId, int requiredSpace);
+        Task UpdateZoneUsageAsync(int zoneId, int spaceUsed);
+
+
+    }
+}
