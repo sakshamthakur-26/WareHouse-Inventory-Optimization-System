e3c9e2953272cf49a648a8f39e8f237a8f6493e4
Singh <2492266@cognizant.com>
2026-05-26 18:09:30 +0530

    just commit

--- WareHouse Optimization System/obj/Debug/net8.0/WareHouse Optimization System.AssemblyInfo.cs
+++ WareHouse Optimization System/obj/Debug/net8.0/WareHouse Optimization System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WareHouse Optimization System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+154b34ec35fbf64471c2555a587e5f12308c8a6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6ad90d9283d64529e55b3f1e38d2b9ea8839a8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WareHouse Optimization System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WareHouse Optimization System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
