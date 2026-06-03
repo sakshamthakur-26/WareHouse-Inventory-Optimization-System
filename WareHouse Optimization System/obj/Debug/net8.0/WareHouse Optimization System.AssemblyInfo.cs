00fb3bbda465ca3837db8295a402215b4ad8da79
saksham <sakshamt2025@gmail.com>
2026-06-04 00:18:32 +0530

    add low stock alter

--- WareHouse Optimization System/obj/Debug/net8.0/WareHouse Optimization System.AssemblyInfo.cs
+++ WareHouse Optimization System/obj/Debug/net8.0/WareHouse Optimization System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WareHouse Optimization System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+509d385dbfe60882ae2a2b711a050fff36b652b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+269995fd4a0e16e583096fda903333e0f56e95dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("WareHouse Optimization System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WareHouse Optimization System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
