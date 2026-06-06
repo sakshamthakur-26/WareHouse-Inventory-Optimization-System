7d2f3339edd46660850d10498cae2348a6d9302a
Sumit Patel <2492268@cognizant.com>
2026-06-06 12:51:56 +0530

    added IsActive

--- WareHouse Optimization System/obj/Debug/net8.0/WareHouse Optimization System.AssemblyInfo.cs
+++ WareHouse Optimization System/obj/Debug/net8.0/WareHouse Optimization System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WareHouse Optimization System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9275982f4eae62c6fee56732316455e20de7d164")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65cf362a7663ceed93c9a4ea256a324e8cc02811")]
 [assembly: System.Reflection.AssemblyProductAttribute("WareHouse Optimization System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WareHouse Optimization System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
